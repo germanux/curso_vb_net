d2f87a1f0251ba1b9073b1d9fea923cca831e1c1
German Caballero <gcaballero@pronoide.es>
2017-06-29 19:36:03 +0200

    Trabajando con listas, combos y checkbox

--- App_4_Ejemplo_WinForm_ListBox/App_4_Ejemplo_WinForm_ListBox/FormEventosRaton.vb
+++ App_4_Ejemplo_WinForm_ListBox/App_4_Ejemplo_WinForm_ListBox/FormEventosRaton.vb
@@ -36,6 +36,11 @@
 
         'nuevoBoton = Nothing
     End Sub
+    Private Sub PictureBox1_OnMouseLeave(sender As Object, e As EventArgs) _
+        Handles PictureBox1.MouseLeave, PictureBox1.MouseEnter
+
+        MessageBox.Show("Entrando o saliendo")
+    End Sub
 
 
 End Class
