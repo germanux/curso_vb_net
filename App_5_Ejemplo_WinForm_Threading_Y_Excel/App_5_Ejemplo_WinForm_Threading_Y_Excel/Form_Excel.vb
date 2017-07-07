52dc42daa22d7d090bac3feb0768db00433452e0
German Caballero <gcaballero@pronoide.es>
2017-07-07 20:13:30 +0200

    Proyecto de ejecución multihilo e importación de un Excel.

---
+++ App_5_Ejemplo_WinForm_Threading_Y_Excel/App_5_Ejemplo_WinForm_Threading_Y_Excel/Form_Excel.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form_Excel
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = LectorExcel.DameValorCelda(
+            TextBox1.Text,
+            TextBox2.Text,
+            TextBox3.Text,
+            NumericUpDown1.Value)
+    End Sub
+
+    Private Sub Form_Excel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        TextBox1.Text = "C:\Ejemplo_Caso_programacion_excel.xlsx"
+        TextBox2.Text = "Datos"
+        TextBox3.Text = "E"
+        NumericUpDown1.Value = 4
+    End Sub
+End Class
---
+++ App_5_Ejemplo_WinForm_Threading_Y_Excel/App_5_Ejemplo_WinForm_Threading_Y_Excel/Form_Thread.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Threading
+
+Public Class Form_Thread
+    Private trd1, trd2 As Thread
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        trd1 = New Thread(AddressOf ThreadTask)
+        trd1.IsBackground = True
+        trd1.Start()
+
+        trd2 = New Thread(AddressOf ThreadTask)
+        trd2.IsBackground = True
+        trd2.Start()
+        MessageBox.Show("HA TERMINADO LOAD")
+    End Sub
+
+    Shared contadorHilos As Integer
+
+    Private Async Sub ThreadTask()
+        Dim stp As Integer
+        Dim numeroHilo As Integer
+        Dim rnd As New Random()
+        contadorHilos = contadorHilos + 1
+        numeroHilo = contadorHilos
+
+        stp = rnd.Next(3000, 6000)
+        Thread.Sleep(stp)
+        MessageBox.Show("HAN PASADO " & stp & " ms EN HILO " & numeroHilo)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("This is the main thread")
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+End Class
