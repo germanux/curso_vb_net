b0de01bef82bc1bef7da3f47cfec0038e2f00bb9
German Caballero <gcaballero@pronoide.es>
2017-07-13 20:02:23 +0200

    Construcción de comandos con parámetros

--- App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosAccess.vb
+++ App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosAccess.vb
@@ -32,13 +32,46 @@ Namespace Modelo
 
                         Dim sql As String
                         sql = "INSERT INTO empleado ( Nombre, Apellidos, Genero, Categoria, " _
-                            & "Retribucion_Fija ) VALUES ( '" _
-                            & empleado.nombre & "', '" & empleado.apellidos & "', " _
-                            & empleado.genero & ", " & empleado.categoria & ", " _
-                            & empleado.retribucionFija & " ) "
+                            & "Retribucion_Fija ) VALUES (@nombre, @apellidos, @genero, @categoria, @retribucionFija )"
+
+                        ''" _
+                        '& empleado.nombre & "', '" & empleado.apellidos & "', " _
+                        '& empleado.genero & ", " & empleado.categoria & ", " _
+                        '& empleado.retribucionFija & " ) "
 
                         Console.WriteLine(sql)
                         Dim comando As OleDbCommand = New OleDbCommand(sql, conexionDB)
+
+                        Dim paramNombre As OleDbParameter = New OleDbParameter("@nombre", empleado.nombre)
+                        paramNombre.Direction = ParameterDirection.Input
+                        paramNombre.DbType = DbType.String
+                        paramNombre.Size = 30
+
+                        Dim paramApellidos As OleDbParameter = New OleDbParameter("@apellidos", empleado.apellidos)
+                        paramApellidos.Direction = ParameterDirection.Input
+                        paramApellidos.DbType = DbType.String
+                        paramApellidos.Size = 30
+
+                        Dim paramgenero As OleDbParameter = New OleDbParameter("@genero", empleado.genero)
+                        paramgenero.Direction = ParameterDirection.Input
+                        paramgenero.DbType = DbType.Int32
+                        paramgenero.Size = 30
+
+                        Dim paramcategoria As OleDbParameter = New OleDbParameter("@categoria", empleado.categoria)
+                        paramcategoria.Direction = ParameterDirection.Input
+                        paramcategoria.DbType = DbType.Int32
+                        paramcategoria.Size = 30
+
+                        Dim paramretribucionFija As OleDbParameter = New OleDbParameter("@retribucionFija", empleado.retribucionFija)
+                        paramretribucionFija.Direction = ParameterDirection.Input
+                        paramretribucionFija.DbType = DbType.Single
+                        paramretribucionFija.Size = 30
+
+                        comando.Parameters.Add(paramNombre)
+                        comando.Parameters.Add(paramApellidos)
+                        comando.Parameters.Add(paramgenero)
+                        comando.Parameters.Add(paramcategoria)
+                        comando.Parameters.Add(paramretribucionFija)
 
                         comando.ExecuteNonQuery()
                     Next
@@ -73,15 +106,22 @@ Namespace Modelo
                     ' dataReader.Re
                     Do While dataReader.Read()
                         Dim nuevoEmpleado As New Empleado()
-                        nuevoEmpleado.nombre = dataReader(0)
-                        nuevoEmpleado.apellidos = dataReader(1)
-                        nuevoEmpleado.genero = dataReader(2)
-                        nuevoEmpleado.categoria = dataReader(3)
-                        nuevoEmpleado.retribucionFija = dataReader(4)
+                        nuevoEmpleado.nombre = CType(dataReader(0), String)
+                        nuevoEmpleado.apellidos = dataReader.GetString(1)
+                        nuevoEmpleado.genero = CType(dataReader(2), TipoGenero)
+                        nuevoEmpleado.categoria = CType(dataReader(3), TipoCategoria)
+                        nuevoEmpleado.retribucionFija = CType(dataReader(4), Single)
 
                         listaEmpleados.Add(nuevoEmpleado)
                     Loop
                     dataReader.Close()
+
+                    Dim comando_C As OleDbCommand = conexionDB.CreateCommand()
+                    comando_C.CommandText = "SELECT COUNT(*) FROM Empleado"
+
+                    Dim numEmpleados As Integer = CInt(comando_C.ExecuteScalar())
+                    MessageBox.Show("Se han importado " & numEmpleados & " empleados.")
+
                 Catch ex As Exception
                     MessageBox.Show("Error al importar " & ex.Message)
                 End Try
