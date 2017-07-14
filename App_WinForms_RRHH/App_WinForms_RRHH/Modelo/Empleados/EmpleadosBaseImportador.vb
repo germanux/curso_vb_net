d1598ffa1a1ebdfbf4b4bd524a00ba02b5fbe84f
German Caballero <gcaballero@pronoide.es>
2017-07-14 16:53:34 +0200

    Clase EMpeladosBaseImportador

---
+++ App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosBaseImportador.vb
@@ -0,0 +1,22 @@
+﻿Namespace Modelo
+    Public MustInherit Class EmpleadosBaseImportador
+        Implements IPersistenciaEmpleados
+
+        Private _nombreFichero As String
+        Public Property NombreFichero As String Implements IPersistenciaEmpleados.nombreFichero
+            Get
+                Return _nombreFichero
+            End Get
+            Set(value As String)
+                If (value = "") Then
+                    MessageBox.Show("No se ha establecido el nombre del fichero")
+                Else
+                    _nombreFichero = value
+                End If
+            End Set
+        End Property
+        Public MustOverride Function Exportar(arrayEmpleados() As Empleado) As Boolean Implements IPersistenciaEmpleados.Exportar
+
+        Public MustOverride Function Importar(ByRef arrayEmpleados() As Empleado) As Boolean Implements IPersistenciaEmpleados.Importar
+    End Class
+End Namespace
